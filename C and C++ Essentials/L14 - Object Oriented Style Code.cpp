319792b8233905a507b75e4854d5add3090b185d
Seamus Finlayson <sfinlayson126@gmail.com>
2023-06-27 11:22:28 -0700

    Finish object oriented style code practice

---
+++ C and C++ Essentials/L14 - Object Oriented Style Code.cpp
@@ -0,0 +1,46 @@
+// Author: Seamus Finlayson
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+    private:
+    int length;
+    int width;
+
+    public:
+    Rectangle(int length, int width) {
+        this->length = length;
+        this->width = width;
+    }
+
+    int area() {
+        return length * width;
+    }
+
+    int perimeter () {
+        return length * 2 + width * 2;
+    }
+
+    void setLength(int length) {
+        this->length = length;
+    }
+};
+
+int main() {
+
+    cout << "Running..." << endl;
+
+    Rectangle r(5, 10);
+
+    cout << "Area: " << r.area() << endl;
+    cout << "Perimeter: " << r.perimeter() << endl;
+
+    r.setLength(4);
+    cout << "Area: " << r.area() << endl;
+    cout << "Perimeter: " << r.perimeter() << endl;
+\
+    return 0;
+}
